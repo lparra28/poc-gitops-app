0a8cf0c1ac89204c372a46475f091b6057bd813b
lparra <lparra@imed.cl>
2022-01-13 09:17:19 -0300

    update app

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var server = http.createServer(function(request, response) {
   var path = url.parse(request.url).pathname;
   response.write("<!DOCTYPE html>");
   response.write("<html>");
-  response.write("<head><title>GitOps On Amazon EKS</title><style>body { background-color: yellow; }</style></head>");
+  response.write("<head><title>GitOps On Amazon EKS</title><style>body { background-color: cyan; }</style></head>");
   response.write("<body>");
-  response.write("<h1 style=\"text-align:center\">GitOps on EKS IMED is AWSome!!</h1>");
+  response.write("<h1 style=\"text-align:center\">ArgoCD on IMED!!</h1>");
   response.write("</body>");
   response.write("</html>");
   response.end();
