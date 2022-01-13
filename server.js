27c122920c1e8aad1b847a1216440129d3613810
lparra <lparra@imed.cl>
2022-01-13 09:47:49 -0300

    update app 2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var server = http.createServer(function(request, response) {
   response.write("<html>");
   response.write("<head><title>GitOps On Amazon EKS</title><style>body { background-color: cyan; }</style></head>");
   response.write("<body>");
-  response.write("<h1 style=\"text-align:center\">ArgoCD on IMED!!</h1>");
+  response.write("<h1 style=\"text-align:center\">ArgoCD Hola Mundo!!</h1>");
   response.write("</body>");
   response.write("</html>");
   response.end();
