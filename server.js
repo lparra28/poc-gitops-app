9a485c72c5dc650947196faf9a5c9cbdb28576d0
Luis Parra <88060598+lparra28@users.noreply.github.com>
2022-02-09 16:19:28 -0300

    update background

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var server = http.createServer(function(request, response) {
   var path = url.parse(request.url).pathname;
   response.write("<!DOCTYPE html>");
   response.write("<html>");
-  response.write("<head><title>My App On Amazon EKS</title><style>body { background-color: green; }</style></head>");
+  response.write("<head><title>My App On Amazon EKS</title><style>body { background-color: cyan; }</style></head>");
   response.write("<body>");
   response.write("<h1 style=\"text-align:center\">AWS CI/CD CodePipeline para EKS</h1>");
   response.write("</body>");
